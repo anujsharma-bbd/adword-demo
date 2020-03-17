0df85d4183c8e7c14a9b61b845501f76eb0654c1
Anuj Sharma <sharmaanuj@hotmail.com>
2020-03-18 02:16:24 +0530

    Updates

---
+++ precache-manifest.1461eb188628a12c3434ae96febd7b65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e594a2782f6efa314154cb5302186f42",
+    "url": "/addword-demo/index.html"
+  },
+  {
+    "revision": "299613e1148748c6bf80",
+    "url": "/addword-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f03777664fb518ef7346",
+    "url": "/addword-demo/static/js/2.81e7840b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/addword-demo/static/js/2.81e7840b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299613e1148748c6bf80",
+    "url": "/addword-demo/static/js/main.eb7aaaa6.chunk.js"
+  },
+  {
+    "revision": "547db35de83f506d2274",
+    "url": "/addword-demo/static/js/runtime-main.795d3251.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/addword-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
