637e3f478a05a4c6c771e52d0f68d6cd96bfd8cc
Anuj Sharma <sharmaanuj@hotmail.com>
2020-03-23 18:30:35 +0530

    added dummy data

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -1,5 +1,8 @@
 export const initialState = {
    users: [],
    filters: null,
-   campaignsList: []
+   campaignsList: [{ id: 1, name: 'Campaign 1', userId: 1, startDate: '12/02/2012', endDate: '12/03/2012', active: true, budget: '3k USD' },
+   { id: 2, name: 'Campaign 2', userId: 22, startDate: '12/02/2013', endDate: '12/05/2013', active: true, budget: '3k USD' },
+   { id: 3, name: 'Campaign 3', userId: 2, startDate: '12/02/2014', endDate: '12/06/2014', active: false, budget: '3k USD' },
+   { id: 4, name: 'Campaign 4', userId: 3, startDate: '12/02/2019', endDate: '12/12/2020', active: true, budget: '3k USD' }]
 };
